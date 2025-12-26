3a9dfd7f32fb3e6e1dfc45ab61325dafcb11dbb8
nouser <nouser@localhost.local>
2025-12-26 03:27:13 +0300

    fix: update sha256 for nextai-translator cask

--- Casks/n/nextai-translator.rb
+++ Casks/n/nextai-translator.rb
@@ -1,6 +1,6 @@
 cask "nextai-translator" do
   version "0.6.5"
-  sha256 "bfc6130788bb4d03a8ab6a0b24419437e7378eb63c5840341d7322de77709c52"
+  sha256 "3acc34952ad6928210461e95077bbaf8f28e3e709aa071cf44287fa80e10ad25"
 
   url "https://github.com/nextai-translator/nextai-translator/releases/download/v#{version}/NextAI.Translator_#{version}_aarch64.dmg"
 
