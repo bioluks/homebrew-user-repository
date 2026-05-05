032c1b6f4f2852a44f8109f2cb9635c38512ba22
Meriç Aşkın <askinmeric79@gmail.com>
2026-05-05 23:59:24 +0300

    Fix download URL for Kimi cask

--- Casks/k/kimi.rb
+++ Casks/k/kimi.rb
@@ -2,8 +2,8 @@ cask "kimi" do
   version "3.0.3"
   sha256 "cdcf5bd99d9e8cc5ff47edcb843017ac140a4f5c78980bf2f890299a61bd8809"
 
-  url "https://kimi-img.moonshot.cn/app/download/macos/kimi_#{version}.dmg",
-      verified: "kimi-img.moonshot.cn/app/download/macos/"
+  url "https://kimi-img.moonshot.cn/app/download/mac/kimi_#{version}.dmg",
+      verified: "kimi-img.moonshot.cn/app/download/mac/"
   name "Kimi"
   desc "AI Assistant"
   homepage "https://kimi.moonshot.cn/"
